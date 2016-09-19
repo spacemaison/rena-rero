4e188a89c7b91c5de95135fbcd31c901d4eeaf50
Jesse Tuchsen <tuchsen@hush.ai>
2016-09-19 11:06:44 -0700

    Refactor how rena-rero is built.
    The Babel presets we're moved from the package.json into the gulpfile
    and a react-native field was added to the package.json file. This helps
    react-native use source maps for the project instead of the compiled
    project.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,11 @@ const babel = require('gulp-babel')
 
 gulp.task('default', () => {
   return gulp.src('lib/**/*.js')
-    .pipe(babel())
+    .pipe(babel({
+	  presets: [
+		'babel-preset-es2015',
+		'babel-preset-react'
+	  ]
+	}))
     .pipe(gulp.dest('dist'))
 })
