b71d21ee88a374417e32b43342d871d0dcc9cd7b
Jesse Tuchsen <tuchsen@hush.ai>
2016-09-30 12:32:16 -0700

    Update react-native-drawer-layout to a newer version.
    This fixes an error on iOS that prevented installation on actual devices
    rather than emulators.

--- test/setup.js
+++ test/setup.js
@@ -1,4 +1,9 @@
 import chai from 'chai'
 import sinonChai from 'sinon-chai'
 
+// This fixes a random issue in the 'react-native-drawer-layout' module. I think
+// it's technically not legal to assign to a module import but it works for now.
+import * as react from 'react-native'
+react.Platform = {}
+
 chai.use(sinonChai)
