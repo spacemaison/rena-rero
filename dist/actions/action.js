527430142f1ec72c5d64374a1f85cd6581b5c4cc
Jesse Tuchsen <tuchsen@hush.ai>
2017-01-09 16:07:15 -0800

    Untrack dist directory

--- dist/actions/action.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.action = action;
-function action(type, payload) {
-  return { type: type, payload: payload };
-}
--- dist/actions/actions.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var POP_PAGE = exports.POP_PAGE = 'RENA_RERO_POP_PAGE';
-var PUSH_PAGE = exports.PUSH_PAGE = 'RENA_RERO_PUSH_PAGE';
-var OPEN_DRAWER = exports.OPEN_DRAWER = 'RENA_RERO_OPEN_DRAWER';
-var CLOSE_DRAWER = exports.CLOSE_DRAWER = 'RENA_RERO_CLOSE_DRAWER';
